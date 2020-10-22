d19b46804587e7fd95ac9c02cf7a62deb015437c
priyarnetcon <priya.r@netcon.in>
2020-10-22 23:53:00 +0530

    Initial code move to heroku

--- app/constants/dbprops.js
+++ app/constants/dbprops.js
@@ -1,3 +1,4 @@
 module.exports = {
-    CONNECTION_URL: 'postgres://postgres:root123@localhost:5432/netvp'
+    CONNECTION_URL: 'postgres://postgres:root123@localhost:5432/netvp',
+    PROD_CONNECTION_URL :'postgres://qsnnofvuvdebpx:5a2c2adc2b1d6efa7f22c8231177cf9b1c077930223e2774a42f14045b010eab@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d72hc1e4kt53o2'
 }
--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,6 @@
 const dbparams=require('../constants/dbprops');
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(dbparams.CONNECTION_URL)
+const sequelize = new Sequelize(dbparams.PROD_CONNECTION_URL)
 
 const db= {};
 
